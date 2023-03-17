b04f0c4c83520fa348d2543a6e61249d99c58dbf
Arna2Guardia <117608420+Arna2Guardia@users.noreply.github.com>
2023-03-17 01:54:13 +0100

    Update footer.php

--- ProjectPHP/grp13/items/footer.php
+++ ProjectPHP/grp13/items/footer.php
@@ -9,8 +9,8 @@
         <h5 class="white-text">Links</h5>
         <ul>
           <li><a class="grey-text text-lighten-3" href="https://guardia.school" rel="nofollow" target="_blank">Guardia Cybersecuritty School</a></li>
-          <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/lucas-durand-7768b9251/" rel="nofollow" target="_blank">Lucas DURAND's Linkedin</a></li>
-          <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/matthieu-padovan-b18147252/" rel="nofollow" target="_blank">Matthieu PADOVAN's Linkedin</a></li>
+          <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/" rel="nofollow" target="_blank">Linkedin</a></li>
+          <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/" rel="nofollow" target="_blank">Linkedin</a></li>
         </ul>
       </div>
     </div>
